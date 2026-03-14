907c56ddb381c84c68fe2cc73f7c1a4a6d916966
Juan Damian <juandamiancuervobuitrago@gmail.com>
2026-03-14 11:10:50 -0500

    correcion8

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def calcular_prioridad(dias):
     if dias < 0:
         return "Error: Días abierto no puede ser negativo"
-    if dias >= 7:
+    if dias > 5:
         return "alta"
     return "normal"
