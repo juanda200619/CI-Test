0cee4c7563b8c929f7cb9e4f7beea79f7878fc5e
Juan Damian <juandamiancuervobuitrago@gmail.com>
2026-03-14 10:56:56 -0500

    correcion4

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,13 @@
 from app import calcular_prioridad
 
+
 def test_calcular_prioridad_normal():
     assert calcular_prioridad(3) == "normal"
 
+
 def test_calcular_prioridad_alta():
     assert calcular_prioridad(7) == "alta"
+
 
 def test_dias_negativos():
     expected = "Error: Días abierto no puede ser negativo"
