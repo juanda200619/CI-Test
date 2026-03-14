4c3a5343b299024a0ec4f674d26308ae7a957a01
Juan Damian <juandamiancuervobuitrago@gmail.com>
2026-03-14 10:30:50 -0500

    cambios 2da clase

---
+++ app.py
@@ -0,0 +1,6 @@
+def calcular_prioridad(dias_abierto):
+    
+    if dias_abierto > 5:
+        return "alta"
+    else:
+        return "normal"
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from app import calcular_prioridad
+
+def test_calcular_prioridad_normal():
+    assert calcular_prioridad(3) == "normal"
+
+def test_calcular_prioridad_alta():
+    assert calcular_prioridad(7) == "alta"
+
+def test_dias_negativos():
+    assert calcular_prioridad(-1) == "Error: Días abierto no puede ser negativo"
